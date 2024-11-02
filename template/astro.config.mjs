8fb41fb7a992eea5ea9fd33ef39da4ed46135821
Artjem Nikitin <a.nikitin@space307.com>
2024-11-02 23:07:25 +0100

    update astro assets

--- template/astro.config.mjs
+++ template/astro.config.mjs
@@ -3,5 +3,8 @@ import svelte from "@astrojs/svelte";
 import relativeLinks from "astro-relative-links";
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    assets: "assets",
+  },
   integrations: [svelte(), relativeLinks()],
 });
