16f3491eadc05d1ac79efd902636a35e0d2ce1d8
Artjem Nikitin <a.nikitin@space307.com>
2024-11-02 22:55:14 +0100

    add astro relative links

--- template/astro.config.mjs
+++ template/astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import relativeLinks from "astro-relative-links";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte(), relativeLinks()],
 });
