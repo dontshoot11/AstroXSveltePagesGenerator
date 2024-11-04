a7916ac4e74292b0a09e003cf764a418d92da73c
Artjem Nikitin <a.nikitin@space307.com>
2024-11-04 18:36:20 +0100

    fixed export functions

--- index.js
+++ index.js
@@ -6,12 +6,4 @@ import {
   checkDate,
 } from "astro-x-svelte-static-pages-generator/scripts/helpers/helpers";
 
-import { createTranslationManager } from "astro-x-svelte-static-pages-generator/scripts/translationService/translationUtils";
-export {
-  debounce,
-  throttle,
-  createTranslationManager,
-  getOS,
-  getCookie,
-  checkDate,
-};
+export { debounce, throttle, getOS, getCookie, checkDate };
