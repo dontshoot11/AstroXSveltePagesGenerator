06c2ef0d005f47bb02a0d220ec6abfb1a1a2e01a
Artjem Nikitin <a.nikitin@space307.com>
2025-01-09 15:47:18 +0100

    add astro compress in favor of lonely css nano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,3 @@
 module.exports = {
-  plugins: [
-    require("postcss-nested"),
-    require("autoprefixer"),
-    require("cssnano")({
-      overrideBrowserslist: [
-        "defaults",
-        "last 2 versions",
-        "iOS >= 10",
-        "Safari >= 10",
-      ],
-    }),
-  ],
+  plugins: [require("postcss-nested"), require("autoprefixer")],
 };
--- template/astro.config.mjs
+++ template/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import relativeLinks from "astro-relative-links";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   build: {
     assets: "assets",
   },
-  integrations: [svelte(), relativeLinks()],
+  integrations: [svelte(), relativeLinks(), compress()],
 });
